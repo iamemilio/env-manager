8fb496f09a3d78074c518ab536a9ba943530911f
Emilio Garcia <egarcia@redhat.com>
2019-09-03 17:41:22 -0400

    Completely generic

--- script.sh
+++ script.sh
@@ -1,27 +1,13 @@
+# manage-env program variables
 
-# This is a simple Infra manager
-# Add these scripts to your .bashrc, and customize them based on your environment
+## Path to directory where you want to save metadata for dev environments
+envDir=""
+## Path to file where global envirnment state is stored
+envFile=""
+## Path to shiftstack-ci repo
+ci_dir=""
 
-export STATUS="${STATUS}"
-
-# Variables
-devEnvs="[ shiftstack | upshift | none ]"
-envFile="$HOME/tmp/devEnv.txt"
-
-# The directory where you keep shiftstack-ci
-shiftstack_ci_dir=$HOME/Dev/shiftstack-ci/
-
-# Create a directory that has a sub directory for each environment you want to manage
-# Each of these sub directories should have a clouds.yaml and a cluster-config.yaml
-#
-# Example:
-# 	      clouds __          
-#		\      \	
-#	      upshift   shiftstack
-
-cloud_dir=$HOME/Dev/clouds
-
-# Tool to clean state of local or global env
+# manage-env functions
 clean () {
 	echo "Cleaning your env..."
 	rm -f $HOME/.config/openstack/clouds.yaml
@@ -39,98 +25,145 @@ clean () {
 	echo "Clean!"
 }
 
-# Module for provisioning env to use shiftstack
-shiftstack_provision () {
-	cp -R $cloud_dir/shiftstack/clouds.yaml $HOME/.config/openstack/
-	export OS_CLOUD=openstack
-	cp -R $cloud_dir/shiftstack/cluster_config.sh $shiftstack_ci_dir
-	export STATUS=shiftstack
-	printf "shiftstack" > $envFile
-}
+generic_provision () {
+	env=$1
+	if [ -z $1 ]; then
+		echo "Error: no env passed"
+		break
+	fi
 
-# Module for provisioning env to use upshift
-upshift_provision () {
-	cp -R $cloud_dir/upshift/clouds.yaml $HOME/.config/openstack/
-	export OS_CLOUD=openstack
-	cp -R $cloud_dir/upshift/cluster_config.sh $shiftstack_ci_dir
-	export STATUS=upshift
-	printf "upshift" > $envFile
-}
-
-# Tool to check status of local and global env state
-status () {
-	case $1 in
-		-h)
-			echo ""
-			echo "Environment Status"
-			echo ""
-			echo "status [options]"
-			echo "-h 		show this message"
-			echo "-g		show global environment status"
-			echo "-a 		show status of all environments"
-			;;
-		-g)
-			status="$(cat $envFile)"
-			echo "The status of the global env is: $status"
-			;;
-		-a)
-			status="$(cat $envFile)"
-			echo "The status of the global env is: $status"
-			echo "The status of your env is: ${STATUS}"
-			;;
-		*)
-			echo "The status of your env is: ${STATUS}"
-			;;
+	if [ -d "$envDir/$1/clouds" ]; then
+		cp -R "$envDir/$1/clouds/clouds.yaml" "$HOME/.config/openstack/"
+		cloud=$(cat $envDir/$1/clouds/cloud.txt)
+		export OS_CLOUD=$cloud
+	fi
+	if [ -d "$envDir/$1/clusterConfig" ]; then
+		cp -R "$envDir/$1/clusterConfig/cluster_config.sh" "$ci_dir/cluster_config.sh"
+	fi
+	export STATUS=$1
+	
+	case $2 in
+		-l|--local);;
+		*) printf $1 > $envFile;;
 	esac
 }
 
-# Tool to provision local and global env
-provision () {
+help () {
+	echo ""
+	echo "Manage Env is a tool for provisioning your environment to work with different contexts."
+	echo "Usage: manage-env <[operation]>"
+	echo ""
+	echo "Operations:"
+	echo "help			Print this message."
+	echo "status		Check what env your system is provisioned for."
+	echo "provision		If your env is clean, you can provision it to work with a new context."
+	echo "clean			This will clean your environemet so that it is ready to be re-provisioned."
+	echo "save-env		Save your current env for later use."
+	echo "delete-env	Delete a saved env."
+	echo ""
+}
+ 
+status () {
 	status="$(cat $envFile)"
-        if [ "$status" == "clean" ]; then
-		read -p "Which Dev Environment do you want to provision for: $devEnvs: " provision
-		case $provision in
-			shiftstack|s)
-				shiftstack_provision
-				echo "Please note that this will affect all local dev environments!"
-				;;
-			upshift|u)
-                		upshift_provision
-                		echo "Please note that this will affect all local dev environments!"
-                		;;
-			none|n)
-				echo "Exiting without provisioning..."
-				;;
-			*)
-				echo "Unrecognized input, $provision. Your options are $devEnvs."
-				;;
-		esac
-	else
-	        echo "Env already provisioned for $status. Please clean before provisioning again."
-        fi
+	echo "Global: $status"
+	echo "Local:  ${STATUS}"
 }
 
-# Tool to bring local env up to date with global state
-update () {
-	source $HOME/.bashrc
-}
-
-# Will create the state file if it doesn't exist
-touch $envFile
-
-# Configure terminal env to be consistent with the chosen state
-globalStatus="$(cat $envFile)"
-if  [ -z "$globalStatus" ]; then
-	clean
-elif [ "$globalStatus" != "${STATUS}" ]; then
-	echo "Updating env to match global status"
-	clean -l
-	if [ "$globalStatus" == "shiftstack" ]; then
-		shiftstack_provision
-	elif [ "$globalStatus" == "upshift" ]; then
-                upshift_provision
+global-export () {
+	key="$(echo $1 | cut -d "=" -f 1 )"
+	exists="$(cat $HOME/.bashrc | grep "$key")"
+	if [ -n "$exists" ]; then
+		LINE="$(cat $HOME/.bashrc | grep -n "$key" | cut -d ":" -f 1)"
+		sed -i "${LINE}d" $HOME/.bashrc
 	fi
-	
-	status -a
-fi
+	echo "export $1" >> $HOME/.bashrc
+		source $HOME/.bashrc
+}
 
+provision() {
+	devEnvs="$(ls $envDir)"
+	status="$(cat $envFile)"
+	if [ "$status" == "clean" ]; then
+		echo "Which DEV environment do you want to provision for: "
+		choices="$devEnvs cancel"
+		select choice in $choices;
+		do
+			case $choice in
+				cancel)
+					echo "Exiting."
+					break
+					;;
+				*)
+					generic_provision $choice
+					break
+					;;
+			esac
+		done
+	else
+		echo "Env already provisioned for $status. Please clean before provisioning again."
+	fi
+}
+
+update () {
+	# Configure terminal env to be consistent with the chosen state
+	globalStatus="$(cat $envFile)"
+	if  [ -z "$globalStatus" ]; then
+		clean
+	elif [ "$globalStatus" != "${STATUS}" ]; then
+		echo "Updating env to match global status"
+		clean -l
+		generic_provision $globalStatus -l
+		status
+	fi
+}
+
+save-env () {
+	devEnvs="$(ls $envDir)"
+	echo "What environment do you want to save your state under:"
+	choices="$devEnvs new"
+	TARGET=""
+	select choice in $choices;
+	do
+		# duplicate case impossible
+		if [ "$choice" == "new" ]; then
+			echo -n "enter a name for your env: "
+			read name
+			mkdir $envDir/$name
+			TARGET=$envDir/$name
+			mkdir $TARGET/clouds
+			mkdir $TARGET/clusterConfig
+		else
+			TARGET=$envDir/$choice
+		fi
+
+		if [ -d "$TARGET/clusterConfig" ]; then
+			cp "$ci_dir/cluster_config.sh" $TARGET/clusterConfig/cluster_config.sh
+		fi
+		if [ -d "$TARGET/clouds" ]; then
+			cp $HOME/.config/openstack/clouds.yaml $TARGET/clouds/clouds.yaml
+			echo $OS_CLOUD > $TARGET/clouds/cloud.txt
+		fi
+	break
+	done
+}
+
+delete-env () {
+	devEnvs="$(ls $envDir)"
+	echo "What environment save do you want to delete?"
+	choices="$devEnvs cancel"
+
+	select choice in $choices;
+	do
+		case $choice in
+			cancel)
+				break;;
+			*)
+				rm -rf $envDir/$choice
+				printf "%s deleted!\n" $choice
+				break;;
+		esac
+	done
+}
+
+update
+# More Variables
